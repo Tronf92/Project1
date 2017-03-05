6524559fb15b87b920dfd3b00619bfbaf100c7ec
Adrian Nicorici <Adrian Nicorici>
2017-03-05 17:17:18 +0200

    study

---
+++ Project1/Source.cpp
@@ -0,0 +1,12 @@
+#include <../WinRT/CoreWindow.h>
+#include <Windows.h>
+#include <../vcprojects>
+
+INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int){
+
+	//start win32++
+	CWinApp MyApp;
+
+	CWind MyWindow;
+
+	
